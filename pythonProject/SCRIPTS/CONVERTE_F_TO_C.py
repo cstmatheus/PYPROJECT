da5c9fa4d3a09987124f12c60c8fed44aec400b7
cstmatheus <matheus.gom1055@gmail.com>
2023-10-15 20:01:43 -0400

    atualiza

--- pythonProject/SCRIPTS/CONVERTE_F_TO_C.py
+++ pythonProject/SCRIPTS/CONVERTE_F_TO_C.py
@@ -1,3 +1,7 @@
+"""
+9.	Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+o	C = 5 * ((F-32) / 9).
+"""
 def converte(temp):
     c = 5* ((temp-32)/9)
 
---
+++ pythonProject/SCRIPTS/jogo_dados.py
@@ -0,0 +1,17 @@
+from numpy import random
+
+print('=='*30)
+
+m='JOGO DADOS'
+print('{:^60}'.format(m))
+
+print('=='*30)
+
+print('======= O GANHADOR E AQUELE QUE TIRAR O NUMERO MAIOR =======')
+print('=='*30)
+
+x = random.randint(6)
+
+
+print('[{:^58}]'.format(x+1))
+print('=='*30)
